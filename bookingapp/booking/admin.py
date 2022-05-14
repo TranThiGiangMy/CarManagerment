2b26e6e9d7ffe9f6c5f77838269364840a3e2c3c
TranThiGiangMy <tranmy113cloclo@gmail.com>
2022-05-15 02:44:55 +0700

    my commit

---
+++ bookingapp/booking/admin.py
@@ -0,0 +1,120 @@
+from django.contrib import admin
+from django.contrib.auth.models import Permission
+from django.db.models import Count
+from django.template.response import TemplateResponse
+from django.urls import path
+from django.utils.html import mark_safe
+from .models import User, UserType, UserInfo, Train, Routes, Tickets, Booking, Payment, Bill , Tag, Comment
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class BookingappAdminSite(admin.AdminSite):
+    site_header = "booking app"
+
+    def get_urls(self):
+        return [
+                   path('train_stats/', self.train_stats)
+               ] + super().get_urls()
+
+    def train_stats(self, request):
+        c = Train.objects.count()
+        stats = Train.objects.annotate(train_count=Count('route_trains')).values('id', 'train_count')
+        return TemplateResponse(request, 'admin/train_stats.html', {
+            'c': c,
+            'stats': stats
+        })
+
+
+admin_site = BookingappAdminSite('myapp')
+
+
+class UserInfoForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = UserInfo
+        fields = '__all__'
+
+
+class UserInfoAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name']
+    list_filter = ['name', 'created_date']
+    list_display = ['id', 'name', 'phone', 'created_date']
+    readonly_fields = ['image_view']
+    form = UserInfoForm
+
+    def image_view(self, customer):
+        if customer:
+            return mark_safe(
+                '<img src="/static/{url}" width="120" />' \
+                    .format(url=customer.image.name)
+            )
+
+
+class TrainInLine(admin.StackedInline):
+    model = Train
+    fk_name = 'route_train'
+
+
+class TrainTagInLine(admin.StackedInline):
+    model = Train.tags.through
+
+
+class RoutesAdmin(admin.ModelAdmin):
+    search_fields = ['starting_point', 'ending_point']
+    list_filter = ['starting_point', 'ending_point']
+    list_display = ['id', 'starting_point', 'ending_point', 'distance']
+    inlines = [TrainInLine]
+
+
+class TrainAdmin(admin.ModelAdmin):
+    search_fields = ['starting_date', 'ending_date']
+    list_filter = ['starting_date', 'ending_date']
+    list_display = ['id', 'starting_date', 'ending_date']
+    inlines = [TrainTagInLine]
+
+
+class TicketsAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'price']
+    list_filter = ['price', 'created_date', 'upp_date']
+    list_display = ['id', 'price', 'created_date', 'upp_date']
+
+
+class BookingAdmin(admin.ModelAdmin):
+    search_fields = ['ticket__price', 'number', 'train__date', 'routes_point']  # tìm kiếm
+    list_filter = ['id', 'number', ]
+    list_display = ['id', 'number', 'price', 'point', 'date']  # hiển thị các cột
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    search_fields = ['id']
+    list_filter = ['cash', 'banking']
+    list_display = ['id', 'cash', 'banking']
+
+
+class BillAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'total', 'created_date']
+    list_filter = ['total']
+    list_display = ['id', 'total', 'created_date']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ['content', 'created_date', 'upded_date']
+    list_filter = ['content', 'created_date', 'upded_date']
+    list_display = ['content', 'created_date', 'upded_date']
+
+# # Register your models here.i
+# admin.site.register(Group)
+admin.site.register(Permission)
+admin.site.register(User)
+admin.site.register(UserType)
+admin.site.register(UserInfo, UserInfoAdmin)
+admin.site.register(Train, TrainAdmin)
+admin.site.register(Routes, RoutesAdmin)
+admin.site.register(Tickets, TicketsAdmin)
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Bill, BillAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Tag)
---
+++ bookingapp/booking/models.py
@@ -0,0 +1,130 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import AbstractUser
+
+
+class UserType(models.Model):
+    user_type_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=50, null=False, unique=True)
+
+    def __str__(self):
+        return self.user_type_name
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='uploads/%Y/%m')
+    user_type = models.ForeignKey(UserType, on_delete=models.SET_NULL,
+                                  related_name='users', null=True)
+
+
+class UserInfo(models.Model):
+    # class Meta:
+    #     abstract = True;
+
+    name = models.CharField(max_length=50, null=False, unique=True)
+    phone = models.CharField(max_length=50, null=False, unique=True)
+    image = models.ImageField(upload_to='img_avata/%Y/%m', default=None)
+    created_date = models.DateField(auto_now_add=True)
+    upp_date = models.DateField(auto_now=True)
+    contact = models.CharField(max_length=10)
+    active = models.BooleanField(default=True)  # xóa nhưng vẫn còn dữ liệu
+    routes = models.ManyToManyField('Routes', related_name='routes_user',blank=True)
+    ordering = ["name"]  # sắp sếp
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    content = RichTextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # on_delete: Xóa custommer nhưng vẫn giữ lại đc comment
+    created_date = models.DateField(auto_now_add=True)
+    upded_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return "{0}, {1}".format(self.content, self.content)
+
+
+class Routes(models.Model):
+    objects = None
+
+    class Meta:
+        unique_together = ('starting_point', 'ending_point')
+
+    starting_point = models.CharField(max_length=100, null=False)
+    ending_point = models.CharField(max_length=100, null=False)
+    distance = models.CharField(max_length=100, null=False)
+    note = models.TextField(max_length=50, null=True)
+
+    def __str__(self):
+        return "{0}, {1}".format(self.starting_point, self.ending_point)
+
+
+class Train(models.Model):
+    # lớp cấm trùng
+    class Meta:
+        unique_together = ('starting_date', 'ending_date')
+
+    starting_date = models.DateTimeField()
+    ending_date = models.DateTimeField()
+    tags = models.ManyToManyField('Tag', related_name='tags_trains', blank=True)
+    note = models.TextField(max_length=50, null=True)
+    route_train = models.ForeignKey(Routes, related_name='route_trains', on_delete=models.SET_NULL, null=True)
+    user_train = models.ForeignKey(User, related_name='user_trains', on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return "{0}, {1}".format(self.starting_date, self.ending_date)
+
+
+class Tickets(models.Model):
+    price = models.CharField(max_length=50)
+    created_date = models.DateField(auto_now_add=True)
+    upp_date = models.DateField(auto_now=True)
+    user_ticket = models.ForeignKey(User, related_name='user_tickets', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.price
+
+
+class Booking(models.Model):
+    price = models.ForeignKey(Tickets, related_name='price_booking', on_delete=models.SET_NULL, null=True)
+    number = models.CharField(max_length=50, null=True)
+    point = models.ForeignKey(Routes, related_name='point_booking', on_delete=models.SET_NULL, null=True)
+    date = models.ForeignKey(Train, related_name='date_booking', on_delete=models.SET_NULL, null=True)
+    note = models.TextField(max_length=50, null=True)
+    user_book = models.ForeignKey(User, related_name='user_booking', on_delete=models.SET_NULL, null=True)
+
+
+
+    def __str__(self):
+        return self.number
+
+
+class Payment(models.Model):
+    cash = models.CharField(max_length=50, null=False)
+    banking = models.CharField(max_length=50, null=False)
+    note = models.TextField(max_length=50, null=True)
+    booking = models.ForeignKey(Booking,related_name='booking_payment', on_delete=models.SET_NULL, null=True)
+    user_pay = models.ForeignKey(User, related_name='user_payment', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "{0}, {1}".format(self.cash, self.banking)
+
+
+class Bill(models.Model):
+    total = models.CharField(max_length=50, null=False)
+    created_date = models.DateField(auto_now=True)
+    note = models.TextField(max_length=50, null=True)
+    user_bill = models.ForeignKey(User, related_name='user_bills', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.total)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=58, unique=True)
+
+    def __str__(self):
+        return self.name
